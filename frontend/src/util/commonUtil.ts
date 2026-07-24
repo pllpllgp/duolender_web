1241b2b7422eb0d5a0b979388734602209dd3ce1
yongku <pllpllgp@gmail.com>
2026-07-24 13:48:17 +0900

    front  - 게시글 시분초 제거

--- frontend/src/util/commonUtil.ts
+++ frontend/src/util/commonUtil.ts
@@ -6,9 +6,6 @@ export const formatDateTime = (raw?: string) => {
 	const year = raw.substring(0, 4);
 	const month = raw.substring(4, 6);
 	const day = raw.substring(6, 8);
-	const hour = raw.substring(8, 10);
-	const minute = raw.substring(10, 12);
-	const second = raw.substring(12, 14);
 
 	return `${year}.${month}.${day}`;
 };
